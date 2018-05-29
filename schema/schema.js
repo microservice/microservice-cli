432f92c1f0a62277b5c9fb175949aa01602b207a
Thomas Pedbereznak <tom@tomped.com>
2018-05-29 13:10:53 +0200

    cli name fix and dir fix

--- schema/schema.js
+++ schema/schema.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
+const path = require('path');
 const Ajv = require('ajv');
 
-const schema = JSON.parse(fs.readFileSync('./schema/yaml.json'));
+const schema = JSON.parse(fs.readFileSync(path.join(__dirname, './yaml.json')));
 const ajv = new Ajv({allErrors: true});
 const rawValidate = ajv.compile(schema);
 
