66c763eafc5b2a7bf5a5920d11003ffcc584873c
Thomas Pedbereznak <tom@tomped.com>
2018-06-06 14:58:49 +0200

    Metrics done

--- src/Metrics.js
+++ src/Metrics.js
@@ -1,5 +1,21 @@
 class Metrics {
+  constructor(rawMetrics) {
+    this._ssl = rawMetrics.ssl || false;
+    this._port = rawMetrics.port;
+    this._uri = rawMetrics.uri;
+  }
 
+  isSsl() {
+    return this._ssl;
+  }
+
+  getPort() {
+    return this._port;
+  }
+
+  getUri() {
+    return this._uri;
+  }
 }
 
 module.exports = Metrics;
