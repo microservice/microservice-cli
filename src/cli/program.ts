e05b6f0e1997a3e00f73851462c5954756de9489
Thomas Pedbereznak <tom@tomped.com>
2019-01-28 21:59:35 -0500

    bump

--- src/cli/program.ts
+++ src/cli/program.ts
@@ -8,7 +8,7 @@ const cli = new Cli();
 
 program
   .description('For more details on the commands below, run `omg `(validate|build|run|subscribe|shutdown)` --help`')
-  .version('0.7.2');
+  .version('0.7.3');
 
 program
   .command('validate')
