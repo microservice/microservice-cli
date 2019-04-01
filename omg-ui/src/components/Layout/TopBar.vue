a07c52f2cc9aac5dab1be0576b623b2bac427b98
Aurelien ARINO <aurelien.arino@gmail.com>
2019-04-01 15:31:38 +0200

    feat(omg-ui): add base layout without inactive navigation/router

---
+++ omg-ui/src/components/Layout/TopBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="topbar-container">
+    <div class="topbar-left">
+      <img
+        src="https://avatars2.githubusercontent.com/u/39149433?s=200&v=4"
+        alt="OMG app logo"
+        class="logo"
+      >
+    </div>
+    <div class="topbar-right">
+      <div class="title-container">
+        <h1 class="title">{{ $route.name }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'topbar'
+}
+</script>
+
+<style lang="scss" scoped>
+.topbar-container {
+  width: 100vw;
+  display: flex;
+  height: 88px;
+
+  .topbar-left {
+    width: 300px;
+    min-width: 300px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+
+    img {
+      width: 20%;
+      border-radius: 10px;
+      margin-left: 10px;
+    }
+  }
+
+  .topbar-right {
+    width: 100%;
+    border-bottom: 1px solid lightslategray;
+    height: inherit;
+  }
+}
+</style>
