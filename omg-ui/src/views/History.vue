785ee01086d461b26fa47573ff80d81592e36253
Aurelien ARINO <aurelien.arino@gmail.com>
2019-04-01 17:45:52 +0200

    feat(omg-ui): add history listing
    add History View and Menu

---
+++ omg-ui/src/views/History.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="history-container">
+    <div
+      class="entry"
+      v-for="(entry, entryName) of getHistory"
+      :key="`entry-${entryName}`"
+    >
+      <h2 class="action">{{ entry.action }}</h2>
+      <span class="arg"> {{ entry.args }} </span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'history',
+  data: () => ({}),
+  computed: mapGetters(['getHistory'])
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
