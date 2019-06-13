9bfd1a5aeb9774babe18a354388c2adb660a5e15
Aurelien ARINO <aurelien.arino@gmail.com>
2019-06-13 16:32:58 +0200

    refactor(ong-cli): fix lint issues

--- packages/omg/__tests__/src/models/Microservice.ts
+++ packages/omg/__tests__/src/models/Microservice.ts
@@ -1,8 +1,8 @@
-import Microservice from '../../../src/models/Microservice';
-import Action from '../../../src/models/Action';
-import EnvironmentVariable from '../../../src/models/EnvironmentVariable';
-import Volume from '../../../src/models/Volume';
-import Lifecycle from '../../../src/models/Lifecycle';
+import Microservice from '../../../src/models/Microservice'
+import Action from '../../../src/models/Action'
+import EnvironmentVariable from '../../../src/models/EnvironmentVariable'
+import Volume from '../../../src/models/Volume'
+import Lifecycle from '../../../src/models/Lifecycle'
 
 describe('Microservice.ts', () => {
   describe('constructor', () => {
@@ -13,7 +13,7 @@ describe('Microservice.ts', () => {
           info: {
             version: '1.0.0',
             title: 'test',
-            description: 'for tests',
+            description: 'for tests'
           },
           health: {
             http: {
@@ -21,19 +21,24 @@ describe('Microservice.ts', () => {
               port: 5000
             }
           }
-        });
+        })
       } catch (e) {
         expect(e).toEqual({
-          errors: [{
-            dataPath: '',
-            keyword: 'minProperties',
-            message: 'should NOT have less than 1 properties',
-            params: {limit: 1},
-            schemaPath: '#/minProperties',
-          }], issue: {}, text: 'data should NOT have less than 1 properties', valid: false,
-        });
+          errors: [
+            {
+              dataPath: '',
+              keyword: 'minProperties',
+              message: 'should NOT have less than 1 properties',
+              params: { limit: 1 },
+              schemaPath: '#/minProperties'
+            }
+          ],
+          issue: {},
+          text: 'data should NOT have less than 1 properties',
+          valid: false
+        })
       }
-    });
+    })
 
     test('throws an exception because a command interfaces via http and a lifecycle is not provided', () => {
       try {
@@ -42,7 +47,7 @@ describe('Microservice.ts', () => {
           info: {
             version: '1.0.0',
             title: 'test',
-            description: 'for tests',
+            description: 'for tests'
           },
           health: {
             http: {
@@ -52,23 +57,24 @@ describe('Microservice.ts', () => {
           },
           actions: {
             foo: {
-              output: {type: 'map'},
+              output: { type: 'map' },
               http: {
                 method: 'post',
                 port: 5000,
-                path: '/data',
-              },
-            },
-          },
-        });
+                path: '/data'
+              }
+            }
+          }
+        })
       } catch (e) {
         expect(e).toEqual({
           context: 'Action with name: `foo`',
-          message: 'If an action interfaces with http then a lifecycle must be provided',
-        });
+          message:
+            'If an action interfaces with http then a lifecycle must be provided'
+        })
       }
-    });
-  });
+    })
+  })
 
   describe('.rawData', () => {
     test('gets the raw data after validation', () => {
@@ -77,7 +83,7 @@ describe('Microservice.ts', () => {
         info: {
           version: '1.0.0',
           title: 'test',
-          description: 'for tests',
+          description: 'for tests'
         },
         health: {
           http: {
@@ -88,23 +94,30 @@ describe('Microservice.ts', () => {
         actions: {
           foo: {
             format: {
-              command: 'foo.sh',
+              command: 'foo.sh'
             },
             output: {
-              type: 'boolean',
-            },
-          },
-        },
-      });
+              type: 'boolean'
+            }
+          }
+        }
+      })
 
       expect(m.rawData).toEqual({
         errors: null,
         text: 'No errors',
         valid: true,
-        yaml: {actions: {foo: {format: {command: 'foo.sh'}, output: {type: 'boolean'}}}, health: {http: {path: '/health', port: 5000}}, omg: 1, info: {description: 'for tests', title: 'test', version: '1.0.0'}},
-      });
-    });
-  });
+        yaml: {
+          actions: {
+            foo: { format: { command: 'foo.sh' }, output: { type: 'boolean' } }
+          },
+          health: { http: { path: '/health', port: 5000 } },
+          omg: 1,
+          info: { description: 'for tests', title: 'test', version: '1.0.0' }
+        }
+      })
+    })
+  })
 
   describe('.commands', () => {
     test('gets the empty command list', () => {
@@ -113,7 +126,7 @@ describe('Microservice.ts', () => {
         info: {
           version: '1.0.0',
           title: 'test',
-          description: 'for tests',
+          description: 'for tests'
         },
         health: {
           http: {
@@ -121,10 +134,10 @@ describe('Microservice.ts', () => {
             port: 5000
           }
         }
-    });
+      })
 
-      expect(m.actions).toEqual([]);
-    });
+      expect(m.actions).toEqual([])
+    })
 
     test('gets the command list', () => {
       const m = new Microservice({
@@ -132,7 +145,7 @@ describe('Microservice.ts', () => {
         info: {
           version: '1.0.0',
           title: 'test',
-          description: 'for tests',
+          description: 'for tests'
         },
         health: {
           http: {
@@ -143,22 +156,31 @@ describe('Microservice.ts', () => {
         actions: {
           foo: {
             format: {
-              command: 'foo.sh',
+              command: 'foo.sh'
             },
-            output: {type: 'boolean'},
+            output: { type: 'boolean' }
           },
           bar: {
             format: {
-              command: 'bar.sh',
+              command: 'bar.sh'
             },
-            output: {type: 'int'},
-          },
-        },
-      });
+            output: { type: 'int' }
+          }
+        }
+      })
 
-      expect(m.actions).toEqual([new Action('foo', {format: {command: 'foo.sh'}, output: {type: 'boolean'}}), new Action('bar', {format: {command: 'bar.sh'}, output: {type: 'int'}})]);
-    });
-  });
+      expect(m.actions).toEqual([
+        new Action('foo', {
+          format: { command: 'foo.sh' },
+          output: { type: 'boolean' }
+        }),
+        new Action('bar', {
+          format: { command: 'bar.sh' },
+          output: { type: 'int' }
+        })
+      ])
+    })
+  })
 
   describe('.getCommand(command)', () => {
     const m = new Microservice({
@@ -166,7 +188,7 @@ describe('Microservice.ts', () => {
       info: {
         version: '1.0.0',
         title: 'test',
-        description: 'for tests',
+        description: 'for tests'
       },
       health: {
         http: {
@@ -177,24 +199,29 @@ describe('Microservice.ts', () => {
       actions: {
         foo: {
           format: {
-            command: 'foo.sh',
+            command: 'foo.sh'
           },
-          output: {type: 'list'},
-        },
-      },
-    });
+          output: { type: 'list' }
+        }
+      }
+    })
     test('throws and exception because the command does not exist', () => {
       try {
-        m.getAction('bar');
+        m.getAction('bar')
       } catch (e) {
-        expect(e).toEqual('Action: `bar` does not exist');
+        expect(e).toEqual('Action: `bar` does not exist')
       }
-    });
+    })
 
     test('gets the command', () => {
-      expect(m.getAction('foo')).toEqual(new Action('foo', {format: {command: 'foo.sh'}, output: {type: 'list'}}));
-    });
-  });
+      expect(m.getAction('foo')).toEqual(
+        new Action('foo', {
+          format: { command: 'foo.sh' },
+          output: { type: 'list' }
+        })
+      )
+    })
+  })
 
   describe('.environmentVariables', () => {
     test('gets the empty environment variable list', () => {
@@ -203,7 +230,7 @@ describe('Microservice.ts', () => {
         info: {
           version: '1.0.0',
           title: 'test',
-          description: 'for tests',
+          description: 'for tests'
         },
         health: {
           http: {
@@ -211,10 +238,10 @@ describe('Microservice.ts', () => {
             port: 5000
           }
         }
-    });
+      })
 
-      expect(m.environmentVariables).toEqual([]);
-    });
+      expect(m.environmentVariables).toEqual([])
+    })
 
     test('gets the environment variable list', () => {
       const m = new Microservice({
@@ -222,7 +249,7 @@ describe('Microservice.ts', () => {
         info: {
           version: '1.0.0',
           title: 'test',
-          description: 'for tests',
+          description: 'for tests'
         },
         health: {
           http: {
@@ -232,17 +259,20 @@ describe('Microservice.ts', () => {
         },
         environment: {
           foo: {
-            type: 'boolean',
+            type: 'boolean'
           },
           bar: {
-            type: 'string',
-          },
-        },
-      });
+            type: 'string'
+          }
+        }
+      })
 
-      expect(m.environmentVariables).toEqual([new EnvironmentVariable('foo', {type: 'boolean'}), new EnvironmentVariable('bar', {type: 'string'})]);
-    });
-  });
+      expect(m.environmentVariables).toEqual([
+        new EnvironmentVariable('foo', { type: 'boolean' }),
+        new EnvironmentVariable('bar', { type: 'string' })
+      ])
+    })
+  })
 
   describe('.areRequiredEnvironmentVariablesSupplied(environmentVariableMapping)', () => {
     const m = new Microservice({
@@ -250,7 +280,7 @@ describe('Microservice.ts', () => {
       info: {
         version: '1.0.0',
         title: 'test',
-        description: 'for tests',
+        description: 'for tests'
       },
       health: {
         http: {
@@ -261,23 +291,25 @@ describe('Microservice.ts', () => {
       environment: {
         foo: {
           type: 'boolean',
-          required: true,
+          required: true
         },
         bar: {
-          type: 'string',
-        },
-      },
-    });
+          type: 'string'
+        }
+      }
+    })
     test('returns true because all required environment variables are supplied', () => {
-      expect(m.areRequiredEnvironmentVariablesSupplied({
-        foo: false,
-      })).toBeTruthy();
-    });
+      expect(
+        m.areRequiredEnvironmentVariablesSupplied({
+          foo: false
+        })
+      ).toBeTruthy()
+    })
 
     test('returns false because not all required environment variables are supplied', () => {
-      expect(m.areRequiredEnvironmentVariablesSupplied({})).toBeFalsy();
-    });
-  });
+      expect(m.areRequiredEnvironmentVariablesSupplied({})).toBeFalsy()
+    })
+  })
 
   describe('.requiredEnvironmentVariables', () => {
     test('gets the list of required environment variables', () => {
@@ -286,7 +318,7 @@ describe('Microservice.ts', () => {
         info: {
           version: '1.0.0',
           title: 'test',
-          description: 'for tests',
+          description: 'for tests'
         },
         health: {
           http: {
@@ -297,17 +329,17 @@ describe('Microservice.ts', () => {
         environment: {
           foo: {
             type: 'boolean',
-            required: true,
+            required: true
           },
           bar: {
-            type: 'string',
-          },
-        },
-      });
+            type: 'string'
+          }
+        }
+      })
 
-      expect(m.requiredEnvironmentVariables).toEqual(['foo']);
-    });
-  });
+      expect(m.requiredEnvironmentVariables).toEqual(['foo'])
+    })
+  })
 
   describe('.volumes', () => {
     test('gets the empty volume list', () => {
@@ -316,7 +348,7 @@ describe('Microservice.ts', () => {
         info: {
           version: '1.0.0',
           title: 'test',
-          description: 'for tests',
+          description: 'for tests'
         },
         health: {
           http: {
@@ -324,10 +356,10 @@ describe('Microservice.ts', () => {
             port: 5000
           }
         }
-    });
+      })
 
-      expect(m.volumes).toEqual([]);
-    });
+      expect(m.volumes).toEqual([])
+    })
 
     test('gets the volume list', () => {
       const m = new Microservice({
@@ -335,7 +367,7 @@ describe('Microservice.ts', () => {
         info: {
           version: '1.0.0',
           title: 'test',
-          description: 'for tests',
+          description: 'for tests'
         },
         health: {
           http: {
@@ -345,21 +377,24 @@ describe('Microservice.ts', () => {
         },
         volumes: {
           foo: {
-            target: '/foo',
+            target: '/foo'
           },
           bar: {
             target: '/bar',
-            persist: true,
-          },
-        },
-      });
+            persist: true
+          }
+        }
+      })
 
-      expect(m.volumes).toEqual([new Volume('foo', {target: '/foo'}), new Volume('bar', {
-        target: '/bar',
-        persist: true,
-      })]);
-    });
-  });
+      expect(m.volumes).toEqual([
+        new Volume('foo', { target: '/foo' }),
+        new Volume('bar', {
+          target: '/bar',
+          persist: true
+        })
+      ])
+    })
+  })
 
   describe('.getVolume(volume)', () => {
     const m = new Microservice({
@@ -367,7 +402,7 @@ describe('Microservice.ts', () => {
       info: {
         version: '1.0.0',
         title: 'test',
-        description: 'for tests',
+        description: 'for tests'
       },
       health: {
         http: {
@@ -377,22 +412,22 @@ describe('Microservice.ts', () => {
       },
       volumes: {
         foo: {
-          target: '/foo',
-        },
-      },
-    });
+          target: '/foo'
+        }
+      }
+    })
     test('throws and exception because the volume does not exist', () => {
       try {
-        m.getVolume('bar');
+        m.getVolume('bar')
       } catch (e) {
-        expect(e).toEqual({message: 'Volume: `bar` does not exist'});
+        expect(e).toEqual({ message: 'Volume: `bar` does not exist' })
       }
-    });
+    })
 
     test('gets the volume', () => {
-      expect(m.getVolume('foo')).toEqual(new Volume('foo', {target: '/foo'}));
-    });
-  });
+      expect(m.getVolume('foo')).toEqual(new Volume('foo', { target: '/foo' }))
+    })
+  })
 
   describe('.lifecycle', () => {
     test('gets the lifecycle', () => {
@@ -401,12 +436,12 @@ describe('Microservice.ts', () => {
         info: {
           version: '1.0.0',
           title: 'test',
-          description: 'for tests',
+          description: 'for tests'
         },
         lifecycle: {
           startup: {
-            command: ['node', 'app.js', 'foo'],
-          },
+            command: ['node', 'app.js', 'foo']
+          }
         },
         health: {
           http: {
@@ -414,13 +449,15 @@ describe('Microservice.ts', () => {
             port: 5000
           }
         }
-    });
+      })
 
-      expect(m.lifecycle).toEqual(new Lifecycle({
-        startup: {
-          command: ['node', 'app.js', 'foo'],
-        },
-      }));
-    });
-  });
-});
+      expect(m.lifecycle).toEqual(
+        new Lifecycle({
+          startup: {
+            command: ['node', 'app.js', 'foo']
+          }
+        })
+      )
+    })
+  })
+})
