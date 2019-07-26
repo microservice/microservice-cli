b78e79a538c7b880ce0a48cf30a926186972a26f
Aurelien ARINO <aurelien.arino@gmail.com>
2019-07-26 18:46:03 +0200

    ci(travis): remove npx, it takes too much time

--- tests/regression/travis.sh
+++ tests/regression/travis.sh
@@ -7,7 +7,7 @@ export PATH="$DIR/bin/linux:$PATH"
 echo $PATH
 
 echo "===== OMG VERSION ====="
-npx omg -V
+omg -V
 
 echo "===== HELLO WORLD TEMPLATES ===="
 mkdir repos
@@ -16,7 +16,7 @@ for repo in clojure d go node java python php ruby rust scala ; do
     echo "===== Testing $repo ====="
     git clone -q --depth 1 "https://github.com/microservices/${repo}.git"
     cd "$repo"
-    output="$(npx omg run --raw message -a name="Peter" | jq -c .)"
+    output="$(omg run --raw message -a name="Peter" | jq -c .)"
     if [ "$output" != '{"message":"Hello Peter"}' ] ; then echo "$output"; exit 1; fi
     cd ..
 done
@@ -41,21 +41,21 @@ echo "===== TESTING BASIC SERVICES ====="
 
 echo "===== Testing base64 ====="
 cd base64
-output="$(npx omg run --raw encode -a content="Peter")"
+output="$(omg run --raw encode -a content="Peter")"
 if [ "$output" != 'UGV0ZXI=' ] ; then echo "$output"; exit 1; fi
-output="$(npx omg run --raw decode -a content="UGV0ZXI=")"
+output="$(omg run --raw decode -a content="UGV0ZXI=")"
 if [ "$output" != 'Peter' ] ; then echo "$output"; exit 1; fi
 
 echo "===== Testing hashes ====="
 cd ../hashes
-output="$(npx omg run --raw digest -a method="sha1" -a data="foo" | jq -c .)"
+output="$(omg run --raw digest -a method="sha1" -a data="foo" | jq -c .)"
 if [ "$output" != '{"method":"sha1","digest":"0BEEC7B5EA3F0FDBC95D0DD47F3C5BC275DA8A33"}' ] ; then echo "$output"; exit 1; fi
-output="$(npx omg run --raw hmac -a method="sha1" -a data="hello world" -a secret="my secret" | jq -c .)"
+output="$(omg run --raw hmac -a method="sha1" -a data="hello world" -a secret="my secret" | jq -c .)"
 if [ "$output" != '{"method":"sha1","digest":"9F60EE4B05E590A7F3FAC552BFB9D98FA46F78D9"}' ] ; then echo "$output"; exit 1; fi
 
 # echo "===== Testing jwt ====="
 # cd ../jwt
-# output="$(npx omg run --raw sign -a data="hello world" -a secret="abc" -a expiresIn="2h" | jq -c .)"
+# output="$(omg run --raw sign -a data="hello world" -a secret="abc" -a expiresIn="2h" | jq -c .)"
 # if [ "$output" != 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiaGVsbG8gd29ybGQiLCJpYXQiOjE1NDM5MjMyMTAsImV4cCI6MTU0MzkzMDQxMH0.FCsstg1m01goffz0cFYxZIUe0uPybUAqzGRnZPJgGBw' ] ; then echo "$output"; exit 1; fi
-# output="$(npx omg run --raw verify -a token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiaGVsbG8gd29ybGQiLCJpYXQiOjE1NDM5MjMyMTAsImV4cCI6MTU0MzkzMDQxMH0.FCsstg1m01goffz0cFYxZIUe0uPybUAqzGRnZPJgGBw" | jq -c .)"
+# output="$(omg run --raw verify -a token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiaGVsbG8gd29ybGQiLCJpYXQiOjE1NDM5MjMyMTAsImV4cCI6MTU0MzkzMDQxMH0.FCsstg1m01goffz0cFYxZIUe0uPybUAqzGRnZPJgGBw" | jq -c .)"
 # if [ "$output" != '{"data":"hello world","iat":1543923210,"exp":1543930410}' ] ; then echo "$output"; exit 1; fi
