fbb1fdfd704eab35007e241e156fa90364e82dfa
StoryScriptAI <anees@storyscript.io>
2019-08-19 13:48:40 +0500

    :new: Implement ExtraHosts for nix

--- packages/omg/src/common.ts
+++ packages/omg/src/common.ts
@@ -1,4 +1,17 @@
 import { CompositeDisposable } from 'event-kit'
+import execa from 'execa'
 
 export const DEBUG_CLI = {}.hasOwnProperty.call(process.env, 'OMG_CLI_DEBUG') || process.argv.includes('--debug')
 export const lifecycleDisposables = new CompositeDisposable()
+
+export async function getHostIp(): Promise<string | null> {
+  const hostDomain = 'host.docker.internal'
+  const { exitCode, stdout } = await execa('ip', ['-4', 'addr', 'show', 'docker0'])
+  if (exitCode === 0) {
+    const outputMatch = stdout.match(/inet ([\d.]+)/)
+    if (outputMatch) {
+      return `${hostDomain}:${outputMatch[1]}`
+    }
+  }
+  return null
+}
--- packages/omg/src/services/docker/getContainer.ts
+++ packages/omg/src/services/docker/getContainer.ts
@@ -2,6 +2,7 @@ import _get from 'lodash/get'
 import getPort from 'get-port'
 import Dockerode from 'dockerode'
 
+import { getHostIp } from '~/common'
 import { ConfigSchema } from '~/types'
 import { getContainerPorts } from '~/services/config'
 
@@ -18,6 +19,7 @@ interface GetContainerResult {
   portsMap: Map<number, number>
 }
 
+const SHOULD_MAP_TO_LOCALHOST = !['darwin', 'win32'].includes(process.platform)
 export default async function getContainer({ config, envs, image }: GetContainerOptions): Promise<GetContainerResult> {
   const imageWithTag = image.includes(':') ? image : `${image}:latest`
 
@@ -64,16 +66,15 @@ export default async function getContainer({ config, envs, image }: GetContainer
     portBindings[`${containerPort}/tcp`] = [{ HostPort: freePort.toString() }]
   }
 
+  const hostIp = SHOULD_MAP_TO_LOCALHOST ? await getHostIp() : null
   const container = await dockerode.createContainer({
     Image: image,
     Cmd: _get(config, 'lifecycle.startup.command', null),
     Env: Object.entries(envObj).map(([k, v]) => `${k}=${v}`),
     ExposedPorts: portsExposed,
-    // ^ Exposed ports are container ports
     HostConfig: {
       PortBindings: portBindings,
-      // TODO: Make ExtraHosts work on linux
-      // Lookup `getHostIp` in old codebase
+      ExtraHosts: SHOULD_MAP_TO_LOCALHOST || !hostIp ? [] : [hostIp],
     },
   })
 
