7e983568906302aa5cf59b6f13b8806381ddf884
StoryScriptAI <anees@storyscript.io>
2019-08-29 09:51:09 +0500

    :art: Minor cleanup for waitForHttpPortOpen

--- packages/omg/src/helpers/waitForHttpPortOpen.ts
+++ packages/omg/src/helpers/waitForHttpPortOpen.ts
@@ -6,24 +6,23 @@ export default async function waitForHttpPortOpen(port: number, attempts: number
   const attemptsToUse = Math.min(10, attempts)
 
   function attemptConnection() {
-    return new Promise((resolve, reject) => {
+    return new Promise(resolve => {
       const request = http.get(`http://localhost:${port}/`, {
         timeout: WAIT_MS,
       })
       request.once('response', () => {
-        resolve()
+        resolve(true)
         request.end()
       })
-      request.once('error', reject)
+      request.once('error', () => {
+        resolve(false)
+      })
     })
   }
 
   for (let i = 0; i < attemptsToUse; i += 1) {
-    try {
-      await attemptConnection()
+    if (await attemptConnection()) {
       return true
-    } catch (_) {
-      /* No Op */
     }
     await new Promise(resolve => setTimeout(resolve, WAIT_MS))
   }
