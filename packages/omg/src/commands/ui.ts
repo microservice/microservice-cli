fbf5e9794820efdcf9d740acb4387fde1e1e499a
StoryScriptAI <anees@storyscript.io>
2019-08-29 09:58:07 +0500

    :new: Add struct for dashboard service

--- packages/omg/src/commands/ui.ts
+++ packages/omg/src/commands/ui.ts
@@ -1,3 +1,5 @@
+import { Dashboard } from '~/services/dashboard'
+import { getConfigPaths, parseMicroserviceConfig } from '~/services/config'
 import { CommandPayload, CommandOptionsDefault } from '~/types'
 
 interface ActionOptions extends CommandOptionsDefault {
@@ -7,5 +9,18 @@ interface ActionOptions extends CommandOptionsDefault {
 }
 
 export default async function ui({ options }: CommandPayload<ActionOptions>) {
-  console.log('UI command')
+  const configPaths = await getConfigPaths(options, true)
+  const microserviceConfig = await parseMicroserviceConfig({
+    configPath: configPaths.microservice,
+    validate: true,
+  })
+
+  const dashboard = new Dashboard({
+    configPaths,
+    microserviceConfig,
+  })
+  dashboard.start({
+    port: parseInt(options.port || '', 10) || null,
+    inheritEnv: !!options.inheritEnv,
+  })
 }
---
+++ packages/omg/src/services/dashboard/Dashboard.ts
@@ -0,0 +1,41 @@
+import { ConfigSchema } from '~/types'
+import { ConfigPaths } from '~/services/config'
+import { lifecycleDisposables } from '~/common'
+
+interface DashboardOptions {
+  configPaths: ConfigPaths
+  microserviceConfig: ConfigSchema
+}
+
+interface DashboardStartOptions {
+  port: number | null
+  inheritEnv: boolean
+}
+
+export default class Dashboard {
+  public configPaths: ConfigPaths
+  public microserviceConfig: ConfigSchema
+
+  public constructor(options: DashboardOptions) {
+    this.configPaths = options.configPaths
+    this.microserviceConfig = options.microserviceConfig
+
+    lifecycleDisposables.add(this)
+  }
+
+  public async start(options: DashboardStartOptions): Promise<void> {
+    // TODO
+    console.log('hello world')
+  }
+
+  public async stop(): Promise<void> {
+    // TODO
+  }
+
+  public terminate(): void {}
+
+  public dispose() {
+    lifecycleDisposables.delete(this)
+    this.terminate()
+  }
+}
