8592de62abaaab99dc85929acad36e2c73ec35e5
StoryScriptAI <anees@storyscript.io>
2019-09-13 20:58:54 +0500

    :art: Minor tweaks to standalone build script

--- scripts/build-standalone.sh
+++ scripts/build-standalone.sh
@@ -16,7 +16,8 @@ OUTPUT_NAME="omg"
 OUTPUT_NODE_VERSION="10.16.0"
 OUTPUT_PLATFORMS="alpine-x64 alpine-x86 linux-x64 linux-x86 mac-x64 windows-x64 windows-x86"
 
+rm -rf built/*
 for PLATFORM in $OUTPUT_PLATFORMS; do
   OUTPUT_TARGET="$PLATFORM-$OUTPUT_NODE_VERSION"
-  build_package --name $OUTPUT_NAME --target $OUTPUT_TARGET --output built/omg-$OUTPUT_TARGET
+  build_package --name $OUTPUT_NAME --target $OUTPUT_TARGET --output built/omg-$PLATFORM
 done
