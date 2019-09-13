a184814297bbdba2e37e11a1e615edf5be77fa69
StoryScriptAI <anees@storyscript.io>
2019-09-13 20:46:18 +0500

    :new: Add script for building standalone binaries

---
+++ scripts/build-standalone.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Navigate to repo root
+cd $(dirname $0)/..
+
+function build_package {
+  yarn nexe packages/omg/lib/cli.js \
+            -r packages/omg/package.json \
+            -r packages/omg-ui/package.json \
+            -r packages/omg-ui/lib/ \
+            -r packages/omg-validate/package.json \
+            -r packages/omg-validate/lib/ $@
+}
+
+OUTPUT_NAME="omg"
+OUTPUT_NODE_VERSION="10.16.0"
+OUTPUT_PLATFORMS="alpine-x64 alpine-x86 linux-x64 linux-x86 mac-x64 windows-x64 windows-x86"
+
+for PLATFORM in $OUTPUT_PLATFORMS; do
+  OUTPUT_TARGET="$PLATFORM-$OUTPUT_NODE_VERSION"
+  build_package --name $OUTPUT_NAME --target $OUTPUT_TARGET --output built/omg-$OUTPUT_TARGET
+done
