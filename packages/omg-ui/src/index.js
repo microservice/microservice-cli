c434ff1b4979a56eb9a6ff5d2549a80c532fd137
StoryScriptAI <anees@storyscript.io>
2019-09-19 05:20:08 +0500

    :fire: vue-router

--- packages/omg-ui/src/index.js
+++ packages/omg-ui/src/index.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import OMG from './views/index.vue'
-import router from './router'
 import store from './store'
 
 import 'normalize.css'
 
 new Vue({
-  router,
   store,
   render: h => h(OMG),
 }).$mount('#app')
