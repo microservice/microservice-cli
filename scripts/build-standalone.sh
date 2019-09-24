06d33f55b250504383bf7f2c24d32887cbf04d1a
StoryScriptAI <anees@storyscript.io>
2019-09-25 00:18:30 +0200

    :new: Attempt to use zeit/pkg instead of nexe

--- scripts/build-standalone.sh
+++ scripts/build-standalone.sh
@@ -3,21 +3,23 @@
 # Navigate to repo root
 cd $(dirname $0)/..
 
+if [ "$(which pkg)" = "" ]; then
+  echo "zeit/pkg is not installed on your system or not in your \$PATH"
+  echo "Try doing 'yarn global add zeit' and then re-run this script"
+  exit 1
+fi
+
 function build_package {
-  yarn nexe packages/omg/lib/cli.js \
-            -r packages/omg/package.json \
-            -r packages/omg-ui/package.json \
-            -r packages/omg-ui/lib/ \
-            -r packages/omg-validate/package.json \
-            -r packages/omg-validate/lib/ $@
+  pkg packages/omg/lib/cli.js $@
 }
 
 OUTPUT_NAME="omg"
-OUTPUT_NODE_VERSION="10.16.0"
-OUTPUT_PLATFORMS="alpine-x64 alpine-x86 linux-x64 linux-x86 mac-x64 windows-x64 windows-x86"
+OUTPUT_NODE_VERSION="10"
+OUTPUT_PLATFORMS="linux-x64 macos-x64 win-x64"
 
 rm -rf built/*
 for PLATFORM in $OUTPUT_PLATFORMS; do
-  OUTPUT_TARGET="$PLATFORM-$OUTPUT_NODE_VERSION"
-  build_package --name $OUTPUT_NAME --target $OUTPUT_TARGET --output built/omg-$PLATFORM
+  OUTPUT_TARGET="node$OUTPUT_NODE_VERSION-$PLATFORM"
+  echo "Generating $OUTPUT_TARGET"
+  build_package --target $OUTPUT_TARGET --output built/omg-$PLATFORM
 done
