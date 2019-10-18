ed00a3c409261177e20e742435e4b6a01799e428
StoryScriptAI <anees@storyscript.io>
2019-10-18 11:14:55 +0500

    :bug: Workaround parcel bug for prod builds in Dropdown

--- packages/omg-ui/src/icons/Dropdown.vue
+++ packages/omg-ui/src/icons/Dropdown.vue
@@ -1,7 +1,7 @@
 
 <template>
-  <svg viewBox="0 0 8 5" fill="none" xmlns="http://www.w3.org/2000/svg" v-bind:style="svgStyle">
-    <path d="M4 4.66675L0 0.666748H8L4 4.66675Z" v-bind:fill="fillColor" />
+  <svg viewBox="0 0 8 5" fill="none" xmlns="http://www.w3.org/2000/svg" :style="svgStyle">
+    <path d="M4 4.66675L0 0.666748H8L4 4.66675Z" :fill="fillColor" />
   </svg>
 </template>
 
