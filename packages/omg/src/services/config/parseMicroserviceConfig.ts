6a4fd516d894e7b9fd8f2201ce6e1796cce372ca
StoryScriptAI <anees@storyscript.io>
2019-10-19 18:12:01 +0500

    :art: Improve messaging in malformed config files

--- packages/omg/src/services/config/parseMicroserviceConfig.ts
+++ packages/omg/src/services/config/parseMicroserviceConfig.ts
@@ -12,13 +12,27 @@ interface ParseMicroserviceConfigOptions {
 }
 
 export default async function parseMicroserviceConfig(options: ParseMicroserviceConfigOptions): Promise<ConfigSchema> {
-  const parsed = jsYaml.safeLoad(await fs.readFile(options.configPath, 'utf8'))
+  let parsed
+  let errorPosition: string | null = null
+
+  try {
+    parsed = jsYaml.safeLoad(await fs.readFile(options.configPath, 'utf8'))
+  } catch (error) {
+    if (error && error.name === 'YAMLException' && error.mark) {
+      errorPosition = ` at Line ${error.mark.line} at Column ${error.mark.column}`
+    }
+  }
+
   if (typeof parsed !== 'object' || !parsed) {
-    throw new CLIError(`Malformed config file (found at: ${options.configPath})`)
+    throw new CLIError(
+      `Malformed config file (found at: ${options.configPath})\nIt's probably because of a syntax error${errorPosition} in the Config file.`,
+    )
   }
   if (options.validate) {
     if (getValidationErrors(parsed).length) {
-      logger.fatal(`Config file has errors (found at: ${options.configPath})`)
+      logger.fatal(
+        `Config file has errors (found at: ${options.configPath})\nRun 'omg validate' to get a detailed explanation.`,
+      )
     }
   }
 
